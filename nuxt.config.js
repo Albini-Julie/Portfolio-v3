4e29005dc16a7bbe97f03b09b5e942bdb090fe9e
Julie <julie.albini@edu.univ-fcomte.fr>
2024-04-14 22:09:31 +0200

    feat(paramétrage) : paramétrage du projet (variables, mixins, fonctions, pages, components, fonts)

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  components: true,
+  css: ['@/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/scss/foundations/_variables.scss";
+          @import "@/scss/foundations/_mixins.scss";`
+        }
+      }
+    }
+  },
+})

1d955e326e410317ea0b5d37e9aadc8cfd5cb4f5
Julie <julie.albini@edu.univ-fcomte.fr>
2024-04-15 18:33:40 +0200

    feat(index) : intégration du header

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
-  <h1 class="titre">Bonjour</h1>
-  <h1 class="titre_2">Bonjour</h1>
+  <div class="index">
+    <Header home />
+  </div>
 </template>
-
-<style scoped lang="scss">
-.titre {
-  font-family: $primary-font-family;
-  font-weight: 200;
-  &_2 {
-    font-family: $secondary-font-family;
-  }
-}
-</style>

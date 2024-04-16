9dd3eae7ea71870b8b5ea81ca9604f171996bc71
Julie <julie.albini@edu.univ-fcomte.fr>
2024-04-16 22:19:20 +0200

    feat(projets) : mise en place d'un début d'animation du caroussel

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 import { ref } from 'vue';
 
 export var slide = ref(0);
+export var anime = ref(false)
--- pages/projets.vue
+++ pages/projets.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="caroussel">
     <div class="slides">
-      <Projet color="#FFD700" colorText="#000000" v-if="slide === 0" />
-      <Projet color="#105A13" colorText="#FFFFFF" v-if="slide === 1" />
-      <Projet color="#16105A" colorText="#FFFFFF" v-if="slide === 2" />
-      <Projet color="#970051" colorText="#FFFFFF" v-if="slide === 3" />
+      <Projet
+        :style="{ zIndex: slide === 0 ? 50 : 1 }"
+        color="#FFD700"
+        colorText="#000000"
+        v-if="slide === 0"
+        :class="{ 'slide-animation-1': slide === 0 }"
+      />
+      <Projet
+        :style="{ zIndex: slide === 1 ? 50 : 1 }"
+        color="#105A13"
+        colorText="#FFFFFF"
+        v-if="slide === 1"
+        :class="{ 'slide-animation-2': slide === 1 }"
+      />
+      <Projet
+        :style="{ zIndex: slide === 2 ? 50 : 1 }"
+        color="#16105A"
+        colorText="#FFFFFF"
+        v-if="slide === 2"
+        :class="{ 'slide-animation-3': slide === 2 }"
+      />
+      <Projet
+        :style="{ zIndex: slide === 3 ? 50 : 1 }"
+        color="#970051"
+        colorText="#FFFFFF"
+        v-if="slide === 3"
+        :class="{ 'slide-animation-4': slide === 3 }"
+      />
     </div>
   </div>
 </template>
@@ -13,8 +37,81 @@
 .slides {
   height: 100vh;
 }
+
+.slide-animation-1 {
+  animation: slideFromLeft 0.5s ease-out forwards;
+}
+
+.slide-animation-2 {
+  animation: slideFromLeft 0.5s ease-out forwards;
+}
+
+.slide-animation-3 {
+  animation: slideFromLeft 0.5s ease-out forwards;
+}
+
+.slide-animation-4 {
+  animation: slideFromLeft 0.5s ease-out forwards;
+}
+
+@keyframes slideFromTop {
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slideToLeft {
+  0% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  100% {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+}
+
+@keyframes slideFromLeft {
+  0% {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slideFromRight {
+  0% {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
 </style>
 
 <script setup>
+import { ref } from "vue";
 import { slide } from "~/config";
+
+const BackColor = ref("");
+
+if (slide === 0) {
+  BackColor.value = "#FFD700";
+} else if (slide === 1) {
+  BackColor.value = "#105A13";
+} else if (slide === 2) {
+  BackColor.value = "#16105A";
+} else if (slide === 2) {
+  BackColor.value = "#16105A";
+}
 </script>

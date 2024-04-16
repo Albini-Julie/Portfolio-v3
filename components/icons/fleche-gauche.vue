84e08913a202349ca041f1fd968c491610550c28
Julie <julie.albini@edu.univ-fcomte.fr>
2024-04-16 19:10:06 +0200

    feat(projets) : intégration de la card projet

---
+++ components/icons/fleche-gauche.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="50"
+    height="28"
+    viewBox="0 0 50 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M13.8362 27.7297L16.0912 25.481L6.08995 15.4574L50 15.4574V12.2723L6.08995 12.2723L16.0912 2.24872L13.8362 2.86102e-05L0 13.8649L13.8362 27.7297Z"
+      fill="black"
+    ></path>
+  </svg>
+</template>

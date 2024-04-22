b6426d1c43b275d6a743540342c63d51982ae91e
Julie <julie.albini@edu.univ-fcomte.fr>
2024-04-22 19:52:24 +0200

    feat(id) : intégration complète et responsive en statique de la page id

---
+++ components/icons/vue.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    width="80"
+    height="80"
+    viewBox="0 0 80 80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style="width: 80px; height: 80px; position: relative"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <g clip-path="url(#clip0_114_708)">
+      <path
+        d="M62.8844 6.3739L40.092 45.9099L17.1171 6.37089L30.3417 6.37589L39.5935 21.7328L40.0225 22.4449L40.4505 21.7321L49.6692 6.37718L62.8844 6.3739Z"
+        fill="#252525"
+        stroke="#FFFAF4"
+      ></path>
+      <path
+        d="M0.569336 5.98038L16.2356 5.87344L40.08 46.9097L63.7375 5.87594L79.4312 5.89238L40.0775 73.4299L0.569336 5.98038Z"
+        fill="#252525"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0_114_708">
+        <rect width="80" height="80" fill="white"></rect>
+      </clipPath>
+    </defs>
+  </svg>
+</template>
